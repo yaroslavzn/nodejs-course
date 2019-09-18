0f2dabc0f026f25d5c0045fb7b2c38b7818c2cd1
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2019-09-18 20:51:37 +0300

    start building weather app

---
+++ weather-app/app.js
@@ -0,0 +1,11 @@
+console.log("Start app.");
+
+setTimeout(() => {
+  console.log("2 Second Timer");
+}, 2000);
+
+setTimeout(() => {
+  console.log("0 Second Timer");
+}, 0);
+
+console.log("End app.");
