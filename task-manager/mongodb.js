bb69053943443424d89cb46b97fbdef44b08e1c0
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2019-09-25 08:21:40 +0300

    add db connection and insert first document

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -3,3 +3,20 @@ const MongoClient = mongodb.MongoClient;
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const database = "task-manager";
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to the DB.");
+    }
+
+    const db = client.db(database);
+
+    db.collection("users").insertOne({
+      name: "Yaroslav",
+      age: 26
+    });
+  }
+);
