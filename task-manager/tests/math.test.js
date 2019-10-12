b9c7550554cbb422739a50b3c78fdfe42c4ec232
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2019-10-12 14:41:01 +0300

    start write test code for the task manager app

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,8 @@
-const { calculateTip } = require("../src/math");
+const {
+  calculateTip,
+  fahrenheitToCelsius,
+  celsiusToFahrenheit
+} = require("../src/math");
 
 test("Should calculate tip", () => {
   const total = calculateTip(10, 0.3);
@@ -8,4 +12,16 @@ test("Should calculate tip", () => {
 test("Should calculate tip (with default tip value)", () => {
   const total = calculateTip(10);
   expect(total).toBe(15);
+});
+
+test("Should convert 32 F to 0 C", () => {
+  const total = fahrenheitToCelsius(32);
+
+  expect(total).toBe(0);
+});
+
+test("Should convert 0 C to 32 F", () => {
+  const total = celsiusToFahrenheit(0);
+
+  expect(total).toBe(32);
 });
